48cf44fa87a76e5b28f37f94c2a4d8822c4e03a9
AliAgayevv <aghayev_233@mail.ru>
2024-10-25 17:00:26 +0400

    addition, subtraction, multiplication, and division DONE

--- public/script.js
+++ public/script.js
@@ -1,8 +1,69 @@
 const display = document.getElementById("display");
 const clearScreen = document.getElementById("clearScreen");
 const numberButtons = document.querySelectorAll(".numberBtn");
+const functionButtons = document.querySelectorAll(".functionBtn");
+const showAnswer = document.getElementById("showAnswer");
+
+
 
 display.innerHTML = "0";
+
+var existOperator = false;
+var numberOne;
+var numberTwo;
+var operator;
+
+var answer;
+
+
+
+
+functionButtons.forEach((funcBtn) => {
+    funcBtn.addEventListener('click', () => {
+        if(existOperator === false)
+        {
+            operator = funcBtn.innerText;
+            existOperator = true;
+             numberOne = Number(display.innerHTML);
+            
+            console.log("number 1: ", numberOne)
+            console.log("operator: ", operator)
+            display.innerHTML = `${display.innerHTML}${funcBtn.innerHTML}`
+            if(funcBtn.innerHTML === "=") 
+                {
+                    numberTwo = display.innerHTML.slice(String(numberOne).length + 1, display.innerHTML.length);
+                    alert(numberTwo);
+                }
+        }
+    })
+})
+
+showAnswer.addEventListener('click', () => {
+    numberTwo = Number(display.innerHTML.slice(String(numberOne).length + 1, display.innerHTML.length));
+    switch (operator) {
+        case "÷":
+            answer = numberOne / numberTwo;
+            break;
+    
+        case "×":
+            answer = numberOne * numberTwo;
+            break; 
+
+        case "−":
+            answer = numberOne - numberTwo;
+            break;
+
+        case "+":
+            answer = numberOne + numberTwo;
+            break;
+
+        default:
+            break;
+    }
+    display.innerHTML = answer;
+    answerIsExist = true;
+    existOperator = false;
+})
 
 // Add number to display
 numberButtons.forEach((buttons) => 
@@ -26,14 +87,23 @@ numberButtons.forEach((buttons) =>
 
 // Delete number when DEL button is clicked
 clearScreen.addEventListener('click', () => {
-    lengthOfText = display.innerHTML.length;
-    textWithoutLastElement = display.innerHTML.slice(0, lengthOfText - 1);
-    display.innerText = textWithoutLastElement;
-    if(lengthOfText === 1 )
+    if(!answerIsExist)
+    {
+        lengthOfText = display.innerHTML.length;
+        textWithoutLastElement = display.innerHTML.slice(0, lengthOfText - 1);
+        display.innerText = textWithoutLastElement;
+        if(lengthOfText === 1 )
+        {
+            display.innerHTML = "0";
+            clearScreen.innerHTML = "AC"
+        }
+    }
+    else
     {
         display.innerHTML = "0";
-        clearScreen.innerHTML = "AC"
+        clearScreen.innerHTML= "AC";
     }
+    
 })
 
 // ! Click with command (for the Windows user ctrl), all numbers delete and replace with 0
